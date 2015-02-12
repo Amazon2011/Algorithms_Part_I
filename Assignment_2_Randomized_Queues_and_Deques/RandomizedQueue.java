a8bcbe93e24e97f6df7c66144e259895d768c593
Amazon2011 <chenyongtao2011@gmail.com>
2015-02-12 21:43:25 +0800

    Update Assignment_2_Randomized_Queues_and_Deques code

--- Assignment_2_Randomized_Queues_and_Deques/RandomizedQueue.java
+++ Assignment_2_Randomized_Queues_and_Deques/RandomizedQueue.java
@@ -1,10 +1,66 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
-   public RandomizedQueue()                 // construct an empty randomized queue
-   public boolean isEmpty()                 // is the queue empty?
-   public int size()                        // return the number of items on the queue
-   public void enqueue(Item item)           // add the item
-   public Item dequeue()                    // remove and return a random item
-   public Item sample()                     // return (but do not remove) a random item
-   public Iterator<Item> iterator()         // return an independent iterator over items in random order
-   public static void main(String[] args)   // unit testing
+    private Item[] s;
+    private int size;
+    
+    /**
+     * construct an empty randomized queue
+     */
+    public RandomizedQueue() {
+        
+    }
+    
+    /**
+     * is the queue empty?
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * return the number of items on the queue
+     */
+    public int size() {
+        return size;
+    }
+    
+    /**
+     * add the item
+     */
+    public void enqueue(Item item) {
+        if (item == null) throw new NullPointerException("Item cannot be null!");
+        
+         
+        
+        size++;
+    }
+    
+    /**
+     * remove and return a random item
+     */
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("The RQueue is empty!");
+        
+        
+    }
+    
+    /**
+     * return (but do not remove) a random item
+     */
+    public Item sample() {
+        if (isEmpty()) throw new NoSuchElementException("The RQueue is empty!");
+    }
+    
+    /**
+     * return an independent iterator over items in random order
+     */
+    public Iterator<Item> iterator() {
+        
+    }
+    
+    public static void main(String[] args)  {
+        
+    }
 }
