e565d48b2e85f86f683f010d487e005cc3f55354
Amazon2011 <chenyongtao2011@gmail.com>
2015-04-02 21:59:50 +0800

    Revise Assignment_5_Kd_Trees/

--- Assignment_5_Kd_Trees/KdTree.java
+++ Assignment_5_Kd_Trees/KdTree.java
@@ -1,3 +1,88 @@
 public class KdTree {
+    Node kdTree;
+    int size;
     
+    /**
+     * construct an empty set of points
+     */
+    public KdTree() {
+        
+    }
+    
+    private class Node {
+        Point2D point;
+        Node leftNode, rightNode;
+        bool isVerticalSplitter;
+        
+        Node(Point2D point, bool isVerticalSplitter) {
+            this.point = point;
+            this.isVerticalSplitter = isVerticalSplitter;
+        }
+    }
+    
+    /**
+     * is the set empty?
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * number of points in the set
+     */
+    public int size() {
+        return size;
+    }
+    
+    /**
+     * add the point to the set (if it is not already in the set)
+     */
+    public void insert(Point2D p) {
+        if (p ==  null) throw new NullPointerException("Argument cannot be null!");
+        if (isEmpty()) kdTree = new Node(p, true);
+        else {
+            
+        }
+    }
+    
+    /**
+     * does the set contain point p?
+     */
+    public boolean contains(Point2D p) {
+        if (p ==  null) throw new NullPointerException("Argument cannot be null!");
+        
+    }
+    
+    /**
+     * draw all points to standard draw
+     */
+    public void draw() {
+        
+    }
+    
+    /**
+     * all points that are inside the rectangle
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect ==  null) throw new NullPointerException("Argument cannot be null!");
+        
+        
+    }
+    
+    /**
+     * a nearest neighbor in the set to point p; null if the set is empty
+     */
+    public Point2D nearest(Point2D p) {
+        if (p ==  null) throw new NullPointerException("Argument cannot be null!");
+        if (isEmpty()) return null;
+        
+        
+    }
+
+    /**
+     * unit testing of the methods (optional) 
+     */
+    public static void main(String[] args0) {
+        
+    }
 }
--- Assignment_5_Kd_Trees/PointSET.java
+++ Assignment_5_Kd_Trees/PointSET.java
@@ -1,12 +1,94 @@
-public class PointSET {
-   public         PointSET()                               // construct an empty set of points 
-   public           boolean isEmpty()                      // is the set empty? 
-   public               int size()                         // number of points in the set 
-   public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
-   public           boolean contains(Point2D p)            // does the set contain point p? 
-   public              void draw()                         // draw all points to standard draw 
-   public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle 
-   public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty 
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.ArrayList;
 
-   public static void main(String[] args)                  // unit testing of the methods (optional) 
+public class PointSET {
+    private TreeSet<Point2D> set;
+    
+    /**
+     * construct an empty set of points
+     */
+    public PointSET() {
+        set = new TreeSet();
+    }
+    
+    /**
+     * is the set empty?
+     */
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+    
+    /**
+     * number of points in the set
+     */
+    public int size() {
+        return set.size();
+    }
+    
+    /**
+     * add the point to the set (if it is not already in the set)
+     */
+    public void insert(Point2D p) {
+        if (p ==  null) throw new NullPointerException("Argument cannot be null!");
+        set.add(p);
+    }
+    
+    /**
+     * does the set contain point p?
+     */
+    public boolean contains(Point2D p) {
+        if (p ==  null) throw new NullPointerException("Argument cannot be null!");
+        return set.contains(p);
+    }
+    
+    /**
+     * draw all points to standard draw
+     */
+    public void draw() {
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) iterator.next().draw();
+    }
+    
+    /**
+     * all points that are inside the rectangle
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect ==  null) throw new NullPointerException("Argument cannot be null!");
+        
+        List<Point2D> pointsInRange = new ArrayList<Point2D>();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Point2D point = iterator.next();
+            if (rect.contains(point)) pointsInRange.add(p);
+        }
+        
+        return (Iterable<Point2D>) pointsInRange;
+    }
+    
+    /**
+     * a nearest neighbor in the set to point p; null if the set is empty
+     */
+    public Point2D nearest(Point2D p) {
+        if (p ==  null) throw new NullPointerException("Argument cannot be null!");
+        if (isEmpty()) return null;
+        
+        Iterator iterator = set.iterator();
+        Point2D nearestPoint = iterator.next();
+        
+        while (iterator.hasNext()) {
+            Point2D point = iterator.next();
+            if (point.distanceSquaredTo(p) < nearestPoint.distanceSquaredTo(p)) nearestPoint = point;
+        }
+        
+        return nearestPoint;
+    }
+
+    /**
+     * unit testing of the methods (optional) 
+     */
+    public static void main(String[] args0) {
+        
+    }
 }
