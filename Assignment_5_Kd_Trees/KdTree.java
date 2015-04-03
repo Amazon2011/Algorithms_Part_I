3a9820f3842cea2be7ded129337c41173e7047fb
Amazon2011 <chenyongtao2011@gmail.com>
2015-04-03 22:03:54 +0800

    Revise Assignment_5_Kd_Trees/KdTree.java

--- Assignment_5_Kd_Trees/KdTree.java
+++ Assignment_5_Kd_Trees/KdTree.java
@@ -1,5 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class KdTree {
-    Node kdTree;
+    Node root;
     int size;
     
     /**
@@ -12,11 +16,11 @@ public class KdTree {
     private class Node {
         Point2D point;
         Node leftNode, rightNode;
-        bool isVerticalSplitter;
+        int level;
         
-        Node(Point2D point, bool isVerticalSplitter) {
+        Node(Point2D point, int level) {
             this.point = point;
-            this.isVerticalSplitter = isVerticalSplitter;
+            this.level = level;
         }
     }
     
@@ -39,9 +43,43 @@ public class KdTree {
      */
     public void insert(Point2D p) {
         if (p ==  null) throw new NullPointerException("Argument cannot be null!");
-        if (isEmpty()) kdTree = new Node(p, true);
-        else {
-            
+        if (isEmpty()) {
+            root = new Node(p, 1);
+            size = 1;
+        } else {
+            Node currentNode = root;
+            while (true) {
+                if (p.equals(currentNode.point)) return;
+                if (currentNode.level % 2 == 1) {
+                    if (p.x() < currentNode.point.x()) {
+                        if (currentNode.leftNode == null) {
+                            currentNode.leftNode = new Node(p, currentNode + 1);
+                            size++;
+                            return;
+                        } else currentNode = currentNode.leftNode;
+                    } else {
+                        if (currentNode.rightNode == null) {
+                            currentNode.rightNode = new Node(p, currentNode + 1);
+                            size++;
+                            return;
+                        } else currentNode = currentNode.rightNode;
+                    }
+                } else {
+                    if (p.y() < currentNode.point.y()) {
+                        if (currentNode.leftNode == null) {
+                            currentNode.leftNode = new Node(p, currentNode + 1);
+                            size++;
+                            return;
+                        } else currentNode = currentNode.leftNode;
+                    } else {
+                        if (currentNode.rightNode == null) {
+                            currentNode.rightNode = new Node(p, currentNode + 1);
+                            size++;
+                            return;
+                        } else currentNode = currentNode.rightNode;
+                    }
+                }
+            }
         }
     }
     
@@ -50,7 +88,18 @@ public class KdTree {
      */
     public boolean contains(Point2D p) {
         if (p ==  null) throw new NullPointerException("Argument cannot be null!");
-        
+        Node currentNode = root;
+        while (currentNode != null) {
+            if (p.equals(currentNode.point)) return true;
+            if (currentNode.level % 2 == 1) {
+                if (p.x() < currentNode.point.x()) currentNode = currentNode.leftNode;
+                else currentNode = currentNode.rightNode;
+            } else {
+                if (p.y() < currentNode.point.y()) currentNode = currentNode.leftNode;
+                else currentNode = currentNode.rightNode;  
+            }
+        }
+        return false;
     }
     
     /**
@@ -65,8 +114,37 @@ public class KdTree {
      */
     public Iterable<Point2D> range(RectHV rect) {
         if (rect ==  null) throw new NullPointerException("Argument cannot be null!");
+        List<Point2D> pointsInRange = new ArrayList<Point2D>();
+        if (isEmpty()) return (Iterable<Point2D>) pointsInRange;
         
+        List<Node> checkNodesQueue = new LinkedList<Node>();
+        checkNodesQueue.addLast(root);
         
+        while(!checkNodesQueue.isEmpty()) {
+            Node currentNode = checkNodesQueue.poll();
+            if (rect.contains(currentNode.point)) pointsInRange.add(currentNode.point);
+            if (currentNode.level % 2 == 1) {
+                if (rect.xmin() > currentNode.point.x()) {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                } else if (rect.xmax() < currentNode.point.x()) {
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                } else {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                }
+            } else {
+                if (rect.ymin() > currentNode.point.y()) {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                } else if (rect.ymax() < currentNode.point.y()) {
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                } else {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                }
+            }
+        }
+        
+        return (Iterable<Point2D>) pointsInRange;
     }
     
     /**
@@ -76,6 +154,35 @@ public class KdTree {
         if (p ==  null) throw new NullPointerException("Argument cannot be null!");
         if (isEmpty()) return null;
         
+        Point2D nearestPoint = Point2D(Double.MAX_VALUE, Double.MAX_VALUE);
+        
+        List<Node> checkNodesQueue = new LinkedList<Node>();
+        checkNodesQueue.addLast(root);
+        
+        while(!checkNodesQueue.isEmpty()) {
+            Node currentNode = checkNodesQueue.poll();
+            if (currentNode.point.distanceSquaredTo(p) < nearestPoint.distanceSquaredTo(p)) nearestPoint = currentNode.point;
+            
+            if (currentNode.level % 2 == 1) {
+                if (rect.xmin() > currentNode.point.x()) {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                } else if (rect.xmax() < currentNode.point.x()) {
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                } else {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                }
+            } else {
+                if (rect.ymin() > currentNode.point.y()) {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                } else if (rect.ymax() < currentNode.point.y()) {
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                } else {
+                    if (currentNode.rightNode != null) checkNodesQueue.addLast(currentNode.rightNode);
+                    if (currentNode.leftNode != null) checkNodesQueue.addLast(currentNode.leftNode);
+                }
+            }
+        }
         
     }
 
